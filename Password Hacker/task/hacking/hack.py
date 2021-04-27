df913e79cca2f4393db77739d6e148ca4e92797b
jolayemi_local <ajolayemi82@gmail.com>
2021-04-27 17:04:43 +0200

    Defined cli_parser function

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,1 +1,18 @@
-# write your code here
+#!/usr/bin/env python
+
+import socket
+import argparse
+
+DESCRIPTION = 'Pass in the required arguments. '
+
+
+def cli_parser():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-ip', '--ip_address',
+                        help='Enter the IP address for the admin website')
+    parser.add_argument('--port',
+                        help='Enter the port to be used.')
+    parser.add_argument('-msg', '--message_for_sending',
+                        help='Enter the msg needed to connect to admin website')
+    return parser.parse_args()
+
