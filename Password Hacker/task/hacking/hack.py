a51e8520a16cb8957f52f405dca2094200420902
jolayemi_local <ajolayemi82@gmail.com>
2021-04-27 20:54:32 +0200

    Completed stage 2

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
 import socket
 import argparse
-import sys
+import string
+import random
 
 DESCRIPTION = 'Connects to an unprotected admin website '
+alpha = ''.join((string.ascii_lowercase, string.digits))
 
 
 def cli_parser():
@@ -13,48 +14,36 @@ def cli_parser():
                         help='Enter the IP address for the admin website')
     parser.add_argument('port', type=int,
                         help='Enter the port to be used.')
-    parser.add_argument('msg',
-                        help='Enter the msg needed to connect to admin website')
     return parser.parse_args()
 
 
 class SocketHandler:
     """ Creates and manages both server and clients socket servers. """
 
-    def __init__(self, host, port, msg: str):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.msg = msg.encode('utf-8')
         self.address = (self.host, self.port)
+
+    @staticmethod
+    def gen_pass():
+        return ''.join(random.choice(alpha) for _ in range(random.randint(1, 3)))
 
     def client_socket(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect(self.address)
-            client.sendall(self.msg)
-            server_msg = client.recv(1024)
-        print(server_msg.decode('utf-8'))
-
-    def server_socket(self):
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-                server.bind(self.address)
-                server.listen()
-                con, address = server.accept()
-                with con:
-                    while True:
-                        # Receive data send by user
-                        user_data = server.recv(1024)
-                        if not user_data:
-                            break
-                        server.sendall('Wrong Password!'.encode('utf-8'))
-        except KeyboardInterrupt:
-            sys.exit()
+            while True:
+                generated_pass = self.gen_pass()
+                client.sendall(generated_pass.encode('utf-8'))
+                server_msg = client.recv(1024)
+                if server_msg.decode('utf-8') == 'Connection success!':
+                    print(generated_pass)
+                    break
 
 
 def main():
     args = cli_parser()
-    socket_class = SocketHandler(host=args.ip, port=args.port,
-                                 msg=args.msg)
+    socket_class = SocketHandler(host=args.ip, port=args.port)
     socket_class.client_socket()
 
 
