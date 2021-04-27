5a6d7111f992a87c469a9762c32b368d4f1a12dd
jolayemi_local <ajolayemi82@gmail.com>
2021-04-27 19:17:49 +0200

    Completed stage one

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -2,17 +2,18 @@
 
 import socket
 import argparse
+import sys
 
 DESCRIPTION = 'Connects to an unprotected admin website '
 
 
 def cli_parser():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('-ip', '--ip_address',
+    parser.add_argument('ip',
                         help='Enter the IP address for the admin website')
-    parser.add_argument('--port',
+    parser.add_argument('port', type=int,
                         help='Enter the port to be used.')
-    parser.add_argument('-msg', '--message_for_sending',
+    parser.add_argument('msg',
                         help='Enter the msg needed to connect to admin website')
     return parser.parse_args()
 
@@ -24,22 +25,38 @@ class SocketHandler:
         self.host = host
         self.port = port
         self.msg = msg.encode('utf-8')
-        self.conn_success = False
         self.address = (self.host, self.port)
 
+    def client_socket(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.connect(self.address)
+            client.sendall(self.msg)
+            server_msg = client.recv(1024)
+        print(server_msg.decode('utf-8'))
+
     def server_socket(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-            server.bind(self.address)
-            server.listen()
-            con, address = server.accept()
-            with con:
-                while True:
-                    # Receive data send by user
-                    user_data = server.recv(1024)
-                    if not user_data:
-                        break
-                    server.sendall('Wrong Password!'.encode('utf-8'))
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+                server.bind(self.address)
+                server.listen()
+                con, address = server.accept()
+                with con:
+                    while True:
+                        # Receive data send by user
+                        user_data = server.recv(1024)
+                        if not user_data:
+                            break
+                        server.sendall('Wrong Password!'.encode('utf-8'))
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+def main():
+    args = cli_parser()
+    socket_class = SocketHandler(host=args.ip, port=args.port,
+                                 msg=args.msg)
+    socket_class.client_socket()
 
 
 if __name__ == '__main__':
-    cli_parser()
+    main()
